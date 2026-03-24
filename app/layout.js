822b04ec134ef42ebf8a2e1b034be852379fa4e9
fimifshk <fimif.sh.k@gmail.com>
2026-03-24 15:55:08 +0100

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,45 +1,14 @@
+import "./globals.css";
+
 export const metadata = {
-  title: 'Inspektim Kantiere',
-  description: 'Sistemi GIS për Inspektimet',
-}
+  title: "Inspektim Kantiere",
+  description: "Sistemi GIS për Inspektimet",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="sq">
-      <head>
-        {/* Kjo rregullon renditjen e hartës (TileLayer) */}
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-          crossOrigin=""
-        />
-      </head>
-      <body style={{ margin: 0, padding: 0 }}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
-  )
-export const metadata = {
-  title: 'Inspektim Kantiere',
-  description: 'Sistemi GIS për Inspektimet',
-}
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="sq">
-      <head>
-        {/* Kjo rregullon renditjen e hartës (TileLayer) */}
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-          crossOrigin=""
-        />
-      </head>
-      <body style={{ margin: 0, padding: 0 }}>
-        {children}
-      </body>
-    </html>
-  )
+  );
 }
